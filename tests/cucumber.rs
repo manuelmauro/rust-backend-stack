2ab11941c648cfc9ea40a7f01b17641ac456a01c
Manuel Mauro <manuel.mauro@protonmail.com>
2023-03-09 23:04:50 +0100

    Improve BDD tests

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,30 +1,17 @@
-use cucumber::{when, World as _};
+use cucumber::World as _;
+use reqwest::Response;
 
-use rust_backend_stack::app::users::{NewUser, UserBody};
+mod step_defs;
 
 #[derive(cucumber::World, Debug, Default)]
-struct World {}
-
-#[when(expr = "a user registers as {word} with password {word}")]
-async fn register_user(_w: &mut World, username: String, password: String) {
-    let client = reqwest::Client::new();
-    let res = client
-        .post("http://localhost:8080/api/users")
-        .json(&UserBody {
-            user: NewUser {
-                username: username.clone(),
-                email: format!("{}@example.com", username),
-                password: password,
-            },
-        })
-        .send()
-        .await
-        .expect("Can't reach the server");
-
-    assert!(res.status() == 200)
+struct World {
+    response: Option<Response>,
 }
 
 #[tokio::main]
 async fn main() {
-    World::run("tests/user.feature").await;
+    World::cucumber()
+        .max_concurrent_scenarios(1)
+        .run("tests/features/user.feature")
+        .await;
 }
--- tests/user.feature
+++
@@ -1,4 +0,0 @@
-Feature: User
-
-  Scenario: User registration
-    When a user registers as alice with password pwd_alice
